87a225f075b961c684f0dcb983b23b3040368418
yuki3738 <y.minamiya@gmail.com>
2025-05-23 00:25:27 +0900

    04-06_add-tasks
    https://www.udemy.com/course/learn-vuejs/learn/lecture/28893100#overview

---
+++ 04-06_add-tasks/js/main.js
@@ -0,0 +1,16 @@
+const app = Vue.createApp({
+  data: () => ({
+    newItem: '',
+    todos: []
+  }),
+  methods: {
+    addItem: function(event) {
+      console.log('Clicked!')
+      let todo = {
+        item: this.newItem
+      }
+      this.todos.push(todo)
+    }
+  }
+})
+app.mount('#app')
